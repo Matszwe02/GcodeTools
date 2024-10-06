c87412e7984db54f3d11122097710c1a99a45ae9
Matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2024-10-06 17:40:32 +0200

    Rewrite classes as static

--- gcode_class.py
+++ gcode_class.py
@@ -6,35 +6,21 @@ import re
 
 class Gcode:
 
-    def __init__(self, blocks: BlockList|None = None):
-        
-        self.gcode = ''
-        self.coord_system = CoordSystem()
-        
-        if blocks is not None:
-            self.blocks = blocks
-        else:
-            self.blocks = BlockList()
-
-
-    def from_str(self, gcode_str):
-        self.gcode = gcode_str
-        self.generate_moves()
-        return self
+    def from_str(gcode_str):
+        return Gcode.generate_moves(gcode_str)
     
     
-    def from_file(self, filename: str):
+    def from_file(filename: str):
         with open(filename, 'r') as f:
-            self.from_str(f.read())
-        return self
+            return Gcode.from_str(f.read())
 
 
-    def write_str(self):
+    def write_str(gcode: BlockList):
         out_str = ''
         last_move = None
         i = 0
 
-        for block in tqdm(self.blocks, desc="Writing G-code", unit="line"):
+        for block in tqdm(gcode, desc="Writing G-code", unit="line"):
             i += 1
             command = block.command
             line_str = ''
@@ -55,12 +41,24 @@ class Gcode:
         return out_str
 
 
-    def write_file(self, filename: str):
+    def write_file(gcode: BlockList, filename: str):
         with open(filename, 'w') as f:
-            f.write(self.write_str())
+            f.write(Gcode.write_str(gcode))
+    
+    
+    def log_json(object, filename: str):
+        class CustomEncoder(json.JSONEncoder):
+            def default(self, obj):
+                if hasattr(obj, 'to_dict'):
+                    return obj.to_dict()
+                return super().default(obj)
+        
+        print('Logging json...')
+        with open(filename, 'w') as f:
+            f.write(json.dumps(object, indent=4, cls=CustomEncoder))
 
 
-    def line_to_dict(self, line: str) -> dict[str, str]:
+    def line_to_dict(line: str) -> dict[str, str]:
         line_parts = line.split(';')[0].split()
         if not line_parts:
             return {'0': ''}
@@ -78,7 +76,7 @@ class Gcode:
         return params
 
 
-    def read_meta(self, line: str, meta: dict):
+    def read_meta(line: str, meta: dict):
         if line.startswith('; printing object'):
             meta['object'] = line.removeprefix('; printing object').strip().replace(' ', '_')
         if line.startswith('; stop printing'):
@@ -92,60 +90,63 @@ class Gcode:
         return meta
 
 
-    def generate_moves(self):
-        
-        self.coord_system = CoordSystem()
-        self.blocks:list[Block] = []
+    def generate_moves(gcode_str: str):
+
+        coord_system = CoordSystem()
+        blocks:list[Block] = []
         
         meta = {'object': None, 'type': None, 'line_no': 0}
         
-        gcode_lines = list(filter(str.strip, self.gcode.split('\n')))
+        gcode_lines = list(filter(str.strip, gcode_str.split('\n')))
         for id, line in enumerate(tqdm(gcode_lines, 'Generating moves', unit='line')):
             meta['line_no'] = id
+            meta = Gcode.read_meta(line, meta)
             command = None
             arc = None
             emit_command = False
             
-            line_dict = self.line_to_dict(line)
+            line_dict = Gcode.line_to_dict(line)
             command = line_dict['0']
-            move = Move(self.coord_system)
+            move = Move(coord_system)
             
             if command in ['G0', 'G1', 'G2', 'G3']:
-                move = Move(self.coord_system.copy()).from_params(line_dict)
+                move = Move(coord_system.copy()).from_params(line_dict)
                 
                 if command in ['G2', 'G3']:
                     arc = Arc(dir = int(command[1]), move=move).from_params(line_dict)
             
             elif command in [Static.ABSOLUTE_COORDS, Static.RELATIVE_COORDS]:
-                self.coord_system.set_abs_xyz(command == Static.ABSOLUTE_COORDS)
+                coord_system.set_abs_xyz(command == Static.ABSOLUTE_COORDS)
 
             elif command in [Static.ABSOLUTE_EXTRUDER, Static.RELATIVE_EXTRUDER]:
-                self.coord_system.set_abs_e(command == Static.ABSOLUTE_EXTRUDER)
+                coord_system.set_abs_e(command == Static.ABSOLUTE_EXTRUDER)
 
             elif command == Static.SET_POSITION:
                 vec = Vector().from_params(line_dict)
-                self.coord_system.set_offset(vec)
+                coord_system.set_offset(vec)
             
             elif command == Static.FAN_SPEED:
-                self.coord_system.set_fan(line_dict.get('S', None))
+                coord_system.set_fan(line_dict.get('S', None))
             
             elif command == Static.FAN_OFF:
-                self.coord_system.fan = 0
+                coord_system.fan = 0
             
             elif command in Static.ARC_PLANES.keys():
-                self.coord_system.arc_plane = Static.ARC_PLANES[command]
+                coord_system.arc_plane = Static.ARC_PLANES[command]
             
             else:
                 emit_command = True
             
             command = line.strip()
             
-            new_pos = self.coord_system.apply_move(move.copy())
+            new_pos = coord_system.apply_move(move.copy())
             move.position.set(new_pos)
             
             if arc is not None:
-                gcode_block = Block(arc.copy(), command=command, emit_command=emit_command)
+                gcode_block = Block(arc.copy(), command=command, emit_command=emit_command, meta=meta)
             else:
-                gcode_block = Block(move.copy(), command=command, emit_command=emit_command)
+                gcode_block = Block(move.copy(), command=command, emit_command=emit_command, meta=meta)
             
-            self.blocks.append(gcode_block)
+            blocks.append(gcode_block)
+        
+        return blocks
--- gcode_tools.py
+++ gcode_tools.py
@@ -35,8 +35,8 @@ class Keywords:
     LAYER_CHANGE_START = [(";BEFORE_LAYER_CHANGE", "", "")]
     LAYER_CHANGE_END = [(";AFTER_LAYER_CHANGE", ";TYPE:", "")]
     
-    OBJECT_START = [("EXCLUDE_OBJECT_START", ";TYPE:", "")]
-    OBJECT_END = [("EXCLUDE_OBJECT_END", "", "")]
+    # OBJECT_START = [("EXCLUDE_OBJECT_START", ";TYPE:", "")]
+    # OBJECT_END = [("EXCLUDE_OBJECT_END", "", "")]
     
     GCODE_START = [(";AFTER_LAYER_CHANGE", ";TYPE:", "")]
     GCODE_END = [("EXCLUDE_OBJECT_END", "; EXECUTABLE_BLOCK_END", "")]
@@ -59,41 +59,30 @@ class Keywords:
 
 
 class GcodeTools:
-    
-    def __init__(self, gcode: Gcode):
-        self.gcode = gcode
-        
-        self.start_gcode = None
-        self.end_gcode = None
-        self.object_gcode = None
-        
-        self.precision = 0.02
 
-        self.start_gcode = BlockList()
-        self.end_gcode = BlockList()
-        self.layers = BlockList()
-        self.objects_layers = {}
-
-
-    def read_metadata(self):
-        self.metadata = {}
+    def read_metadata(gcode: BlockList):
+        metadata = {}
         start_id, end_id = -1, -1
-        for id, block in enumerate(self.gcode.blocks):
+        for id, block in enumerate(gcode):
         
-            if start_id == -1 and Keywords.get_keyword_line(id, self.gcode.blocks, Keywords.CONFIG_START): start_id = id
-            if end_id == -1 and Keywords.get_keyword_line(id, self.gcode.blocks, Keywords.CONFIG_END): end_id = id
-                
-        for gcode in self.gcode.blocks[start_id + 1 : end_id]:
-            line = gcode.command
+            if start_id == -1 and Keywords.get_keyword_line(id, gcode, Keywords.CONFIG_START): start_id = id
+            if end_id == -1 and Keywords.get_keyword_line(id, gcode, Keywords.CONFIG_END): end_id = id
+        
+        if start_id == -1 or end_id == -1: return None
+        
+        for block in gcode[start_id + 1 : end_id]:
+            line = block.command
             key = line[1:line.find('=')].strip()
             value = line[line.find('=') + 1:].strip()
-            self.metadata[key] = value
-        return self.metadata
+            metadata[key] = value
+        
+        return metadata
 
 
-
-    def split(self):
-        blocks = self.gcode.blocks
+    def split(gcode: BlockList):
+        """
+        returns (start_gcode: BlockList, end_gcode: BlockList, layers: list[BlockList], objects_layers: dict[list[BlockList]])
+        """
         blocks_bare = BlockList()
         object_gcode = BlockList()
         start_gcode = BlockList()
@@ -103,26 +92,27 @@ class GcodeTools:
         
         start_id, end_id = -1, -1
         
-        for id, block in enumerate(blocks):
-            if start_id == -1 and Keywords.get_keyword_line(id, blocks, Keywords.EXECUTABLE_START): start_id = id
-            if end_id == -1 and Keywords.get_keyword_line(id, blocks, Keywords.EXECUTABLE_END): end_id = id
+        for id, block in enumerate(gcode):
+            if start_id == -1 and Keywords.get_keyword_line(id, gcode, Keywords.EXECUTABLE_START): start_id = id
+            if end_id == -1 and Keywords.get_keyword_line(id, gcode, Keywords.EXECUTABLE_END): end_id = id
             
             if start_id > 0 and end_id > 0:
-                blocks_bare = self.gcode.blocks[start_id : end_id + 1]
+                blocks_bare = gcode[start_id : end_id + 1]
                 break
         
         start_id, end_id = -1, -1
         
-        for id, block in enumerate(blocks_bare):
+        # for id, block in enumerate(blocks_bare):
             
-            if start_id == -1 and Keywords.get_keyword_line(id, blocks_bare, Keywords.OBJECT_START): start_id = id
-            if end_id == -1 and Keywords.get_keyword_line(id, blocks_bare, Keywords.OBJECT_END): end_id = id
+            # if start_id == -1 and Keywords.get_keyword_line(id, blocks_bare, Keywords.OBJECT_START): start_id = id
+            # if end_id == -1 and Keywords.get_keyword_line(id, blocks_bare, Keywords.OBJECT_END): end_id = id
             
-            if start_id > 0 and end_id > 0:
-                object_gcode += blocks_bare[start_id:end_id].copy()
-                object_gcode.g_add(';OBJECT_SPLIT', id)
-                start_id = -1
-                end_id = -1
+            # if start_id > 0 and end_id > 0:
+            # if block.meta.get('object', None) is not None:
+            #     object_gcode += blocks_bare[start_id:end_id].copy()
+            #     object_gcode.g_add(';OBJECT_SPLIT', id)
+            #     start_id = -1
+            #     end_id = -1
         
         start_id, end_id = -1, -1
         for id, block in enumerate(blocks_bare):
@@ -137,20 +127,26 @@ class GcodeTools:
                 end_gcode = blocks_bare[end_id:]
                 print(f'found end gcode at line {id}')
         
-        layer = []
+        layer: BlockList = []
         objects_layer = {}
         
-        for id, block in enumerate(object_gcode):
+        # TODO: Combined enumeration
+        
+        for id, block in enumerate(blocks_bare):
             
-            if Keywords.get_keyword_line(id, object_gcode, Keywords.LAYER_CHANGE_START):
+            if Keywords.get_keyword_line(id, blocks_bare, Keywords.LAYER_CHANGE_START):
                 layers.append(layer)
                 
-                for id in objects_layer.keys():
-                    if id not in objects_layers:
-                        objects_layers[id] = BlockList()
-                    objects_layers[id].append(objects_layer[id])
+                for layer_block in layer:
+                    obj_name = layer_block.meta.get('object', None)
+                    if obj_name is not None:
+                        for id in objects_layer.keys():
+                            if obj_name not in objects_layers and obj_name is not None:
+                                objects_layers[obj_name] = BlockList()
+                            objects_layers[obj_name] = layer.copy()
+                        # objects_layers[id].append(objects_layer[id])
             
-            elif Keywords.get_keyword_line(id, object_gcode, Keywords.LAYER_CHANGE_END):
+            elif Keywords.get_keyword_line(id, blocks_bare, Keywords.LAYER_CHANGE_END):
                 layer = []
                 objects_layer = {}
             else:
@@ -168,36 +164,4 @@ class GcodeTools:
         
         layers.append(layer)
         
-        self.start_gcode = start_gcode
-        self.end_gcode = end_gcode
-        self.layers = layers
-        self.objects_layers = objects_layers
-
-
-    def log_json(self, path='.'):
-        print('Logging json...')
-        class CustomEncoder(json.JSONEncoder):
-            def default(self, obj):
-                if hasattr(obj, 'to_dict'):
-                    return obj.to_dict()
-                return super().default(obj)
-
-        with open(os.path.join(path, 'gcode.json'), 'w') as f:
-            f.write(json.dumps(self.gcode.blocks, indent=4, cls=CustomEncoder))
-            
-        with open(os.path.join(path, 'objects_layers.json'), 'w') as f:
-            f.write(json.dumps(self.objects_layers, indent=4, cls=CustomEncoder))
-            
-        with open(os.path.join(path, 'start_gcode.json'), 'w') as f:
-            f.write(json.dumps(self.start_gcode, indent=4, cls=CustomEncoder))
-            
-        with open(os.path.join(path, 'end_gcode.json'), 'w') as f:
-            f.write(json.dumps(self.end_gcode, indent=4, cls=CustomEncoder))
-            
-        with open(os.path.join(path, 'layers.json'), 'w') as f:
-            f.write(json.dumps(self.layers, indent=4, cls=CustomEncoder))
-            
-        with open(os.path.join(path, 'metadata.json'), 'w') as f:
-            f.write(json.dumps(self.metadata, indent=4))
-        print('Json logged')
-
+        return (start_gcode, end_gcode, layers, objects_layers)
